48e7edfa8c06167ae7b2e56f5a4bea8284bc41af
Matthew Good <m.good13@castleknockcc.ie>
2017-05-15 10:52:19 +0000

    all changes

--- game.js
+++ game.js
@@ -1,25 +1,15 @@
 var isGameOver; 
 var player; 
-var playerImage; 
 var enemy; 
-var enemyImage; 
 var backgroundImage; 
  
  
-function preload() { 
-  playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png"); 
-  enemyImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/OdL0XPt.png"); 
-  backgroundImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/aKQOg3G.png"); 
-} 
- 
- 
+
 function setup() { 
   isGameOver = false; 
-  createCanvas(256, 256); 
-  player = createSprite(width / 2, height - (playerImage.height / 2), 0, 0); 
-  player.addImage(playerImage); 
-  enemy = createSprite(width / 2, 0, 0, 0); 
-  enemy.addImage(enemyImage); 
+  createCanvas(500, 500); 
+  player = createSprite(width / 2, height -25, 50, 50); 
+  enemy = createSprite(width / 2, 0, 10, 30); 
   enemy.rotationSpeed = 4.0; 
 } 
  
@@ -34,21 +24,25 @@ function draw() {
     if (enemy.overlap(player)) { 
       gameOver(); 
     } 
-    background(backgroundImage); 
-    if (keyDown(RIGHT_ARROW) && player.position.x < (width - (playerImage.width / 2))) { 
-      player.position.x += 2; 
+    background(100,100,100);
+    
+    if (keyDown(RIGHT_ARROW) && player.position.x < width) {
+  player.position.x = player.position.x + 2;
+}
     } 
-    if (keyDown(LEFT_ARROW) && player.position.x > (playerImage.width / 2)) { 
-      player.position.x -= 2; 
+    if (keyDown(LEFT_ARROW) && player.position.x > 0) {
+  player.position.x = player.position.x - 1;
+
     } 
     enemy.position.y = enemy.position.y + 3; 
+    
     if (enemy.position.y > height) { 
       enemy.position.y = 0; 
       enemy.position.x = random(5, width - 5); 
     } 
     drawSprites(); 
-  } 
-} 
+}   
+
  
  
 function gameOver() { 
